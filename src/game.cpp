2c89044ed908eacff3e6d1d1eb30d8de0d910bb8
Remy Seassau <r.seassau@eleves-alsacienne.org>
2020-11-19 12:16:54 +0100

    Update game.cpp
    Added Basic Event Handler

--- src/game.cpp
+++ src/game.cpp
@@ -1,1 +1,13 @@
 #include "game.h"
+
+void Game::HandleEvents() {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    switch (event.type) {
+    case SDL_QUIT:
+        is_running_ = false;
+        break;
+    default:
+        break;
+    }
+}
