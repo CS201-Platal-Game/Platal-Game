02309c80173a7c103b9870e07377942cf5a3f13f
Thomas Tressel <thomastressel@gmail.com>
2020-11-22 23:56:48 +0100

    Added a few implementations of object class functions

--- src/object.cpp
+++ src/object.cpp
@@ -1,1 +1,20 @@
 #include "object.h"
+
+Object::Object(int width, int height, bool collidable) {
+    width_ = width;
+    height_ = height;
+    collidable_ = collidable;
+}
+
+int Object::GetWidth() {
+    return width_;
+}
+
+int Object::GetHeight() {
+    return height_;
+}
+
+bool Object::IsCollidable() {
+    return collidable_;
+}
+
