53a7ddd35f72fc2c37d28f58bb7b599173042f3a
christian-roubert <roubert.christian@yahoo.fr>
2020-12-07 00:33:27 +0100

    Changes on the map aspect of the game. This is essentially the start for the map rendering, using the map array and textures.

--- src/platal_map.cpp
+++ src/platal_map.cpp
@@ -1,5 +1,36 @@
 #include "platal_map.h"
+//Chris here changing stuff for the map part, changes still need to be made, 
 
-Map::Map() {}
 
-void Map::DrawMap() {}
+
+Map::Map() {
+
+	// texture1 = TextureManager::LoadTexture("....")
+	// Here we will add as many textures as needed, need yet to be determined
+
+LoadMap(map_array_)
+}
+
+
+void Map::DrawMap(){
+
+	int type = 0;
+
+	for(width = 0; width < width_ ; width +=1)
+	{
+		for(height=0; height <height_ ; height +=1)
+		{
+			type = map_array_[width][height]
+
+			switch(type)
+			{
+				case 0:
+				TextureManager::draw(texture12345etc,src_,dest_)
+				default:
+					break;
+
+			}
+		}
+
+	}
+}
