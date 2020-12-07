49983cfb6a5c59fbfabd3667fb6a3b88bed4f4cc
Remy Seassau <r.seassau@eleves-alsacienne.org>
2020-12-07 03:04:27 +0100

    Removed cyclic dependence between game.h and platal_map.h

--- src/platal_map.h
+++ src/platal_map.h
@@ -3,7 +3,6 @@
 #include "SDL2/SDL_image.h"
 #include "character.h"
 #include "object.h"
-#include "game.h" // added game.h, just in case
 #include <vector>
 // Chris here working on platal_map objects, Since textures are not yet defined the outcome can't be tested, so I am coding for a general idea.
 
@@ -34,5 +33,5 @@ class Map {
     SDL_Rect src_, dest_; // attributes used while rendering
     // SDL_Texture* texture1
     // SDL_Texture* texture2 
-
 };
+
