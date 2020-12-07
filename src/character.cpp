52afffc1fb175ee6346c904d99d83a5265edaab5
Remy Seassau <r.seassau@eleves-alsacienne.org>
2020-12-07 04:39:56 +0100

    Changed move function implimentation

--- src/character.cpp
+++ src/character.cpp
@@ -20,29 +20,27 @@ void Protagonist::HandleInput(SDL_Event event) {
         case SDLK_LEFT:
             orientation_ = kLeft;
             velocity_.yVel = 0;
-            if(velocity_.xVel>0) velocity_.xVel = 0;
-            velocity_.xVel -= 8;
+            velocity_.xVel = 0;
             break;
         
         case SDLK_RIGHT:
             orientation_ = kRight;
             velocity_.yVel = 0;
-            if(velocity_.xVel<0) velocity_.xVel = 0;
-            velocity_.xVel += 8;
+            velocity_.xVel = 0;
             break;
 
         case SDLK_UP:
             orientation_ = kUp;
+            velocity_.yVel = 0;
             velocity_.xVel = 0;
-            if(velocity_.yVel<0) velocity_.yVel = 0;
-            velocity_.yVel += 8;
             break;
 
         case SDLK_DOWN:
             orientation_ = kDown;
+            velocity_.yVel = 0;
             velocity_.xVel = 0;
-            if(velocity_.yVel>0) velocity_.yVel = 0;
-            velocity_.yVel -= 8;
+            break;
+        default:
             break;
     }
 }
@@ -54,41 +52,79 @@ void Protagonist::Move() {
     //Move the protag up or down
     position_.y += velocity_.yVel;
 
-    // update the velocities so they fall back to zero when a key isn't being pressed
-    if( !Game::event_array_[SDLK_LEFT] ){
-        if( velocity_.xVel < 0){
-            velocity_.xVel += 16; // value subject to change
-            // we now treat the case where we overshoot 0
-            if( velocity_.xVel > 0){
-                velocity_.xVel = 0;
+    // The orientation allows us to determine what the last pressed key is
+    switch (orientation_){
+        case kLeft:
+            // update the velocities so they increase if a key is being pressed or fall back to zero when a key isn't being pressed
+            if( Game::event_array_[SDLK_LEFT] ) {
+                if( velocity_.xVel > -64 ) { // We want to limit the velocity in any direction at 64
+                    velocity_.xVel -= 8;
+                    if( velocity_.xVel < -64 ) { // We check after increasing that we are not above the limit
+                        velocity_.xVel = 64;
+                    }
+                }
             }
-        }
-    }
-    
-    if( !Game::event_array_[SDLK_RIGHT] ){
-        if( velocity_.xVel > 0){
-            velocity_.xVel -= 16;
-            if( velocity_.xVel > 0){
-                velocity_.xVel = 0;
+            else if( velocity_.xVel < 0){
+                    velocity_.xVel += 16; // value subject to change
+                    // we now treat the case where we overshoot 0
+                    if( velocity_.xVel > 0){
+                        velocity_.xVel = 0;
+                    }
             }
-        }
-    }
+            break;
+        
+        case kRight:
+            if( Game::event_array_[SDLK_RIGHT] ) {
+                if( velocity_.xVel < 64 ) { 
+                    velocity_.xVel += 8;
+                    if( velocity_.xVel > 64 ) { 
+                        velocity_.xVel = 64;
+                    }
+                }
+            }
+            else if( velocity_.xVel > 0){
+                    velocity_.xVel -= 16; 
+                    if( velocity_.xVel < 0){
+                        velocity_.xVel = 0;
+                    }
+            }
+            break;
+        
+        case kUp:
+            if( Game::event_array_[SDLK_UP] ) {
+                if( velocity_.yVel < 64 ) { 
+                    velocity_.yVel += 8;
+                    if( velocity_.yVel > 64 ) { 
+                        velocity_.yVel = 64;
+                    }
+                }
+            }
+            else if( velocity_.yVel > 0){
+                    velocity_.yVel -= 16; 
+                    if( velocity_.yVel < 0){
+                        velocity_.yVel = 0;
+                    }
+            }
+            break;
 
-    if( !Game::event_array_[SDLK_UP] ){
-        if( velocity_.yVel > 0){
-            velocity_.yVel -= 16;
-            if( velocity_.yVel > 0){
-                velocity_.yVel = 0;
+        case kDown:
+            if( Game::event_array_[SDLK_DOWN] ) {
+                if( velocity_.yVel > -64 ) { 
+                    velocity_.yVel -= 8;
+                    if( velocity_.yVel < -64 ) { 
+                        velocity_.yVel = 64;
+                    }
+                }
             }
-        }
-    }
-
-    if( !Game::event_array_[SDLK_DOWN] ){
-        if( velocity_.yVel < 0){
-            velocity_.yVel += 16;
-            if( velocity_.yVel > 0){
-                velocity_.yVel = 0;
+            else if( velocity_.yVel < 0){
+                    velocity_.yVel += 16; 
+                    if( velocity_.yVel > 0){
+                        velocity_.yVel = 0;
+                    }
             }
-        }
+            break;
+        
+        default:
+            break;
     }
 }
