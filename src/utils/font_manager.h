5aa9348bf3fed99eafa4a0f100f5552c0e473dd8
El-BG-1970 <elouan.gros.fr@gmail.com>
2020-12-10 13:21:37 +0100

    Font manager Class
    red platal game should display on screen if fonts are well installed

---
+++ src/utils/font_manager.h
@@ -0,0 +1,34 @@
+//
+// Font manager class to ease out the process of loading and rendering fonts
+//
+#pragma once
+#include "SDL2/SDL_ttf.h"
+#include <iostream>
+#include <map>
+
+#ifndef PLATAL_GAME_FONT_MANAGER_H
+#define PLATAL_GAME_FONT_MANAGER_H
+
+class FontManager {
+  public:
+    static FontManager* Instance(){
+        if (myInstance == 0)
+            myInstance = new FontManager();
+        return myInstance;
+    }
+
+    // loads a font element
+    bool Load(std::string id, std::string filename, int ptsize);
+    //bool Load(std::string id, std::string filename);
+
+    void Draw(std::string id, std::string txt, int x, int y,
+              SDL_Color, SDL_Renderer*);
+
+  private:
+    std::map<std::string, TTF_Font*> fontMap; // map with all the fonts
+    FontManager() = default;
+    ~FontManager();
+    static FontManager* myInstance;
+};
+
+#endif // PLATAL_GAME_FONT_MANAGER_H
