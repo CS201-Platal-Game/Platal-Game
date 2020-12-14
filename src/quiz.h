3eedf389838d964b1470ebf1f14d1d0b411903e0
CarolinaNinaMatos <carolina2@ninamatos.com>
2020-12-14 12:36:17 +0000

    comments and minor modifications

--- src/quiz.h
+++ src/quiz.h
@@ -6,7 +6,8 @@ class Quiz{
     Quiz(); // basic constructor
     // constructor for 5 questions
     // not sure if we should leave it like this or create a constructor that works for n questions
-    Quiz(Question, Question, Question, Question, Question, int);
+
+    Quiz(Question q1, Question, Question, Question, Question, int); //TODO: make this a vector (n qs)
     void displayScore(); // give total score at the end
     bool pass(); // condition for completing challenge, eg if at least 3/5 questions correct
 
