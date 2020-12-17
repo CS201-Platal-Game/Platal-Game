9024d4f18b5d6aa41a2058f5093b7012d59c7c4c
Duong Dac <dacduong07@gmail.com>
2020-12-17 06:04:14 +0100

    Update event manager

--- src/utils/structs.h
+++ src/utils/structs.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 struct Position {
     int x, y;
 
@@ -26,4 +29,19 @@ struct Position {
 
 struct Velocity {
     int xVel, yVel;
+};
+
+struct Event {
+    unsigned event_id = 0;   // default: null event
+    std::string title;       // "Cours magistral: MAA205".
+    std::string description; // "Tous les étudiants."
+    unsigned start_time;     // in absolute_minute, 930 (15:30)
+    unsigned end_time;       // in absolute_minute, 1050 (17:30)
+};
+
+struct Day {
+    unsigned day_id;
+    std::vector<Event> events;
+    // TODO: Tasks.
+    // TODO: Map modifications.
 };
