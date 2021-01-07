9b8a31044387e94d27f49f155de6cca3b3adbc9d
remyjck <remy.seassau@polytechnique.edu>
2021-01-07 19:02:39 +0100

    Made findtiles

---
+++ src/utils/tiles.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "SDL2/SDL.h"
+#include <vector>
+
+template <typename T> std::vector<SDL_Rect> GetTiles(T item) {
+    std::vector<SDL_Rect> out;
+    SDL_Rect hitbox = item.GetHitbox();
+    for (int x = 0; x < hitbox.w; x += 64) {
+        for (int y = 0; y < hitbox.h; y += 64) {
+            int newX = (hitbox.x + x) / 64;
+            int newY = (hitbox.y + y) / 64;
+            out.push_back({newX, newY, 64, 64});
+        }
+    }
+    return out;
+}
