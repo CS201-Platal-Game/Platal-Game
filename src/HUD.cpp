6e7d83774ef5fd328e563029c51009fffd0a7f3c
christian-roubert <roubert.christian@yahoo.fr>
2021-01-13 17:34:21 +0100

    Fixed HUD

--- src/HUD.cpp
+++ src/HUD.cpp
@@ -8,14 +8,14 @@
 //Chris still here making comments for myself and whoever is reading this
 //Here we have a case where we don't precise the stats, can be changed to a specific situation if needed
 
-HUD::HUD();
+HUD::HUD(){
 	double CurrentGPA = 0;
 	double MaxGPA = 4.2;
 	double CurrentRest = 0;
 	double MaxRest = 100;
 	double CurrentSocial = 0;
 	double MaxSocial = 100;
-
+}
 // Case where the GPA etc... of the character is precised
 
 HUD::HUD(double GPA, double Rest, double Social){
@@ -73,14 +73,14 @@ void HUD::Render() {
 // In order to get what we want I split the total width into 3 for the 3 stats, with 32 length on the outsides, and between the states rendered
 // Tried combining different strings and transforming a double into a string with to_string, to give essentially one sentence that can then be changed depending on stats or preference
 
-	SDL_RECT HUD_rect = {0, 640, 800, 64}
+	SDL_Rect HUD_rect = {0, 640, 800, 64}
 	SDL_SetRenderDrawBlendMode(Game::renderer_, SDL_BLENDMODE_BLEND);
     SDL_SetRenderDrawColor(Game::renderer_, 0, 0, 0, 196);
     SDL_RenderFillRect(Game::renderer_, &HUD_rect);
 
-    GPA_stat_string = "Admin has declared your GPA to be" + std::to_string(GetGPA()) + "out of" + std::to_string(MaxGPA);
-    Rest_stat_string = "Today your body feels" + std::to_string(GetRest()) + "rested out of " + std::to_string(MaxRest);
-    Social_stat_string = "Recent Interactions have filled your social bar to" + std::to_string(GetSocial()) + "out of" + std::to_string(MaxSocial);
+    std::string GPA_stat_string = "Admin has declared your GPA to be" + std::to_string(GetGPA()) + "out of" + std::to_string(MaxGPA);
+    std::string Rest_stat_string = "Today your body feels" + std::to_string(GetRest()) + "rested out of " + std::to_string(MaxRest);
+    std::string Social_stat_string = "Recent Interactions have filled your social bar to" + std::to_string(GetSocial()) + "out of" + std::to_string(MaxSocial);
 
 
 
