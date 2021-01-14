b37f19600dc8449e949aafb95b8d781bea8177bd
ninocapipoca <carolina@ninamatos.com>
2021-01-14 18:37:55 +0000

    Update character.h

--- src/character.h
+++ src/character.h
@@ -1,14 +1,9 @@
 #pragma once
-#include "SDL2/SDL.h"
-#include "SDL2/SDL_image.h"
-#include "utils/structs.h"
-#include "utils/Animation.h"
 #include "dialogue.h"
 #include "stats.h"
 #include <string>
 #include <list>
 #include <vector>
-#include <map>
 
 enum Direction {
     kLeft = 0,
@@ -38,18 +33,15 @@ class Character {
     Position position_{ 0, 0 };
     SDL_Rect hitbox_;
     Direction orientation_; // e.g. protag is facing up/down/etc.
-    int frames_ = 0;
-    int speed_ = 100; //delay between frames, in milliseconds
-
 };
 
 class Protagonist : public Character {
   public:
     // inherit constructors
     Protagonist();
-    Protagonist(const std::string& name, const Position& position);
-    Protagonist(const std::string& name, const Position& position,
-     std::vector<std::pair<std::string, std::string>> idfiles);
+    // files and ids are for animation purposes
+    Protagonist(const std::string& name, const Position& position, vector<string> files, vector<string> ids);
+
     //Takes key presses and adjusts the protag's orientation
     void HandleInput(SDL_Event key);
 
@@ -59,12 +51,21 @@ class Protagonist : public Character {
 
     void Render();
 
-    void CreateAnimationArray(std::vector<std::pair<std::string, std::string>> idfiles);
+    //void CreateAnimationArray(vector<string> files, vector<string> ids);
+
+    Velocity GetVelocity() { return velocity_; }
 
   private:
-    std::vector<AnimatedTexture> animationArray_;
+    // initially size of array shouldn't matter since we're going
+    // to overwrite it anyway
     Position viewport_center_;
+    //Velocity velocity_{ 0, 0 };
     Stats stats_;
+    //Accel accel_ = {1, 1, 1};
+    // TODO: inventories?
+    std::vector<string> animFiles_; // vector of animation strips
+    std::vector<string> animIds_; // vector of IDs of animation strips
+    AnimatedTexture animationArray_[animFiles.size()]; // array has to be same length as nr of files
 };
 
 class NPC : public Character {
