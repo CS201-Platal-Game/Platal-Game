e7f5fc18e118bdc600b5a0545daa0a811232da34
Duong Dac <dacduong07@gmail.com>
2021-01-15 22:26:39 +0100

    Modify toml_parser, minor menu edit

--- src/toml_parser.cpp
+++ src/toml_parser.cpp
@@ -1,32 +1,52 @@
-#include <iostream>
-#include <string>
+#include <assert.h>
+
 #include <fstream>
+#include <iostream>
 #include <map>
+#include <string>
 
-int main () {
-  std::ifstream myfile;
+typedef std::map<std::string, std::map<std::string, std::string>> MapTOML;
 
-  myfile.open("../tomls/tree.toml");
-  if (myfile.fail())
-  	std::cout << "not found" << std::endl;
+const std::string whitespaces = " \t\f\v\n\r";
 
-  std::string s;
-  std::map < std::string, std::map <std::string,std::string> > res;
-  std::string header;
+void StringStrip(std::string& str) {
+    str.erase(0, str.find_first_not_of(whitespaces));
+    str.erase(str.find_last_not_of(whitespaces) + 1);
+}
 
-  while(getline(myfile,s)){
-    if (s.find(" = ") != std::string::npos){
-      int val = s.find(" = ");
-      res[header][s.substr(0,val-1)] = s.substr(val+3);
+// Returns true on success and false on failure.
+bool ParseMapTOML(const std::string& filename, MapTOML& map_toml) {
+    std::ifstream file;
+    file.open(filename.c_str());
+    if (file.fail()) {
+        std::cerr << "File not found" << std::endl;
+        return false;
     }
-    else if (s[0]=='['){
-      header = s.substr(1,s.size()-2);
+    std::string current_line, header = "";
+    while (getline(file, current_line)) {
+        StringStrip(current_line);
+        size_t equal_sign_pos = current_line.find(" = ");
+        if (equal_sign_pos != std::string::npos) {
+            std::string left = current_line.substr(0, equal_sign_pos);
+            std::string right = current_line.substr(equal_sign_pos + 3);
+            StringStrip(left);
+            StringStrip(right);
+            // std::cerr << left << '#' << std::endl;
+            // std::cerr << right << '#' << std::endl;
+            map_toml[header][left] = right;
+        } else if (current_line[0] == '[' && current_line.back() == ']') {
+            header = current_line.substr(1, current_line.size() - 2);
+            StringStrip(header);
+            // std::cerr << header << '#' << std::endl;
+        }
     }
-    else{
-      continue;
-    }
-  }
-  std::cout<<res.size();
-  myfile.close();
-  return 0;
+    file.close();
+    return true;
+}
+
+int main() {
+    std::string filename = "../tomls/tree.toml";
+    MapTOML map_toml;
+    assert(ParseMapTOML(filename, map_toml));
+    return 0;
 }
