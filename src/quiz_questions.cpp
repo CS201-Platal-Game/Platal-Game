b99e9b9d50640da4909c028ceab510cd12580c2c
ninocapipoca <carolina@ninamatos.com>
2021-01-15 09:06:53 +0000

    Loading fonts for quiz and redoing render function
    Turns out I was writing in the wrong place :/

--- src/quiz_questions.cpp
+++ src/quiz_questions.cpp
@@ -4,6 +4,8 @@ Question::Question(const std::string& question_text,
                    const std::string& answer) {
     question_text_ = question_text;
     answer_ = answer;
+    FontManager::Instance()->Load("questionFont", "./fonts/novem___.ttf", 15;
+    FontManager::Instance()->Load("answerFont", "./fonts/novem___.ttf", 12;
 }
 
 void Question::AddOption(const std::string& option) {
@@ -21,9 +23,22 @@ bool Question::CheckCorrect() {
     return false;
 }
 
+
 void Question::RenderQuestion() { // TODO: implement this
     // since we are using keyboard controls this needs to be done such that
     // when an option is selected it changes color
+    SDL_Rect quizBackground = {100, 900, 800, 800}; // x, y (of top left), width, height
+	SDL_SetRenderDrawBlendMode(Game::renderer_, SDL_BLENDMODE_BLEND);
+    SDL_SetRenderDrawColor(Game::renderer_, 209, 207, 198, 100); // light grey
+    SDL_RenderFillRect(Game::renderer_, &quizBackground); // fill 
+    
+    // display the question at the top
+    FontManager::Instance()->Draw("questionFont", question_text_, 150, 750,
+                                  {56, 56, 56}, Game::renderer_); // color: dark grey
+
+
+
+
 }
 
 void Question::PickOption(SDL_Event event) {
